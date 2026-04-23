f329828c3692e236f70413068913206faead5451
ZoltanKost <mikle.mikle200@gmail.com>
2026-04-23 12:11:57 +0200

    forgot header

--- Programs/Praktikum1/Inc/stack.h
+++ Programs/Praktikum1/Inc/stack.h
@@ -5,3 +5,14 @@ typedef struct Stack
 	unsigned int length;
 	unsigned int capacity;
 }
+
+
+int stack_init(unsigned int lenght);
+
+int stack_push(Stack stack, T_token element);
+
+int stack_pop(Stack stack, T_token* resultCopy);
+
+int stack_clear(Stack stack);
+
+int stack_readAt(Stack stack, unsigned int index, T_token* resultCopy);
